179156c6414d6af9f1b71cc9c8778adfb3e7106a
Chandrakala RamaKumari Adabala <217150129+ramakumari1@users.noreply.github.com>
2026-08-19 11:58:57 +0530

    Time: 3 ms (57.57%), Space: 44.5 MB (54.47%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+       int n=s.length();
+       s=s.toLowerCase();
+       int i=0;
+       int j=n-1;
+       while(i<j){
+         while(i<j && !Character.isLetterOrDigit(s.charAt(i))){
+            i++;
+         }
+         while(i<j && !Character.isLetterOrDigit(s.charAt(j))){
+            j--;
+         }
+         if(s.charAt(i)!=s.charAt(j)){
+             return false;
+         }
+         i++;
+         j--;       
+       }
+       return true;
+    }
+}
