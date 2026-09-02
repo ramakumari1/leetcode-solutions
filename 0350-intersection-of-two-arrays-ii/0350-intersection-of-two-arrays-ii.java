1978528e58a9d909c59f09af97209d6cfbf62b50
Chandrakala RamaKumari Adabala <217150129+ramakumari1@users.noreply.github.com>
2026-09-02 13:48:35 +0530

    Time: 3 ms (94.85%), Space: 44.9 MB (77.54%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,35 +1,19 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-       
-        for (int num : nums1) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        int[] arr=new int[Math.min(nums1.length,nums2.length)];
+        HashMap<Integer,Integer>map=new HashMap<>();
+        int index=0;
+        for(int num:nums1){
+            map.put(num,map.getOrDefault(num,0)+1);
         }
-
-        int[] arr = new int[Math.min(nums1.length, nums2.length)];
-        int index = 0;
-
-        
-        for (int num : nums2) {
-
-            if (map.getOrDefault(num, 0) > 0) {
-
-                arr[index] = num;
+        for(int i=0;i<nums2.length;i++){
+            if(map.containsKey(nums2[i]) && map.get(nums2[i]) > 0){
+                arr[index]=nums2[i];
                 index++;
-
-                map.put(num, map.get(num) - 1);
+                map.put(nums2[i],map.get(nums2[i])-1);
             }
         }
-
-       
-        int[] result = new int[index];
-
-        for (int i = 0; i < index; i++) {
-            result[i] = arr[i];
-        }
-
-        return result;
+        return Arrays.copyOf(arr,index);
+    
     }
 }
